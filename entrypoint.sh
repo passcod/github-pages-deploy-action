5dd33e5a5bbb4bfcd84c7e8517815c94050c95cc
Félix Saparelli <felix@passcod.name>
2019-05-16 20:37:19 +1200

    Fix everything

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,78 +1,76 @@
-#!/bin/sh -l
+#!/bin/bash
 
-if [ -z "$ACCESS_TOKEN" ]
+set -euxo pipefail
+
+if [[ -z "$ACCESS_TOKEN" ]]
 then
   echo "You must provide the action with a GitHub Personal Access Token secret in order to deploy."
   exit 1
 fi
 
-if [ -z "$BRANCH" ]
+if [[ -z "$BRANCH" ]]
 then
   echo "You must provide the action with a branch name it should deploy to, for example gh-pages or docs."
   exit 1
 fi
 
-if [ -z "$FOLDER" ]
+if [[ -z "$FOLDER" ]]
 then
   echo "You must provide the action with the folder name in the repository where your compiled page lives."
   exit 1
 fi
 
-if [ -z "$COMMIT_EMAIL" ]
+if [[ -z "$COMMIT_EMAIL" ]]
 then
   COMMIT_EMAIL="${GITHUB_ACTOR}@users.noreply.github.com"
 fi
 
-if [ -z "$COMMIT_NAME" ]
+if [[ -z "$COMMIT_NAME" ]]
 then
   COMMIT_NAME="${GITHUB_ACTOR}"
 fi
 
-# Installs Git.
-apt-get update && \
-apt-get install -y git && \
+# Install git
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y git
 
-# Directs the action to the the Github workspace.
-cd $GITHUB_WORKSPACE && \
+# Direct the action to the GitHub workspace
+cd "$GITHUB_WORKSPACE"
 
-# Configures Git.
-git init && \
-git config --global user.email "${COMMIT_EMAIL}" && \
-git config --global user.name "${COMMIT_NAME}" && \
+# Configure git
+git config --global user.email "$COMMIT_EMAIL"
+git config --global user.name "$COMMIT_NAME"
 
-## Initializes the repository path using the access token.
-REPOSITORY_PATH="https://${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
+## Initialise the repository path using the access token
+git clone --bare "https://${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" source.git
 
-# Checks to see if the remote exists prior to deploying.
-# If the branch doesn't exist it gets created here as an orphan.
-if [ "$(git ls-remote --heads "$REPOSITORY_PATH" "$BRANCH" | wc -l)" -eq 0 ];
-then
-  echo "Creating remote branch ${BRANCH} as it doesn't exist..."
-  git checkout "${BASE_BRANCH:-master}" && \
-  git checkout --orphan $BRANCH && \
-  git rm -rf . && \
-  touch README.md && \
-  git add README.md && \
-  git commit -m "Initial ${BRANCH} commit" && \
-  git push $REPOSITORY_PATH $BRANCH
+# Take a checkout for the source
+git init --separate-git-dir source.git source
+cd source
+git checkout "$BASE_BRANCH" # <- if base branch is empty, will just checkout the default
+cd ..
+
+# Take another checkout for the output
+git init --separate-git-dir source.git output
+cd output
+git checkout $BRANCH || git checkout --orphan $BRANCH
+cd ..
+
+# Run build process in source checkout
+cd source
+mkdir -p "$FOLDER"
+eval "$BUILD_SCRIPT"
+if [[ ! -z "$CNAME" ]]; then
+  echo "$CNAME" > "$FOLDER/CNAME"
 fi
 
-# Checks out the base branch to begin the deploy process.
-git checkout "${BASE_BRANCH:-master}" && \
+# Copy the data to the output checkout
+cd ..
+cp -r "source/$FOLDER"/* "source/$FOLDER"/.* output/
 
-# Builds the project if a build script is provided.
-echo "Running build scripts... $BUILD_SCRIPT" && \
-eval "$BUILD_SCRIPT" && \
-
-if [ "$CNAME" ]; then
-  echo "Generating a CNAME file in in the $FOLDER directory..."
-  echo $CNAME > $FOLDER/CNAME
-fi
-
-# Commits the data to Github.
-echo "Deploying to GitHub..." && \
-git add -f $FOLDER && \
-
-git commit -m "Deploying to ${BRANCH} - $(date +"%T")" && \
-git push $REPOSITORY_PATH `git subtree split --prefix $FOLDER master`:$BRANCH --force && \
-echo "Deployment succesful!"
+# Commit and push
+cd output
+git add -f .
+git commit -m "Deployment Bot (from GitHub Actions)"
+git push origin "$BRANCH"
